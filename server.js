3f4c77d49be850f551cf8b99e0eff28c0fcd4203
MikatekoMarygold <mikatekomarry@gmail.com>
2024-09-21 08:45:56 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -19,20 +19,3 @@ const allowedOrigins = [
     optionsSuccessStatus: 200
   };
 
-  const mongoose = require('mongoose')
-
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/RealEstate")
-    .catch(err => console.log('Something went wrong...', err))
-
-  //Middleware
-app.use(express.json())
-app.use(cors(corsOptions))
-
-// Routes
-const routes = require('./Routes/routes');
-app.use(routes);
-
-app.listen(PORT, ()=> {
-    console.log(`App running on port ${PORT}...`)
-})
